6836bdee8c40322327dd1d32a27b2052e8db950f
heavensward.argentlux <heavensward.argentlux@gmail.com>
2021-04-09 22:07:36 +0800

    untar func done

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import tarfile
 
-def untar(tar_path, out_path='.'):
+def untar(tar_path, out_path='./data'):
 	if tar_path.endswith("tar.gz"):
 		tar = tarfile.open(tar_path, "r:gz")
 		tar.extractall(out_path)
@@ -11,7 +11,7 @@ def untar(tar_path, out_path='.'):
 if __name__ == "__main__":
   print("Running utils.py")
 
-  tar_path = "formula_images.tar.gz"
+  tar_path = "./data/formula_images.tar.gz"
   untar(tar_path)
 
   print("Finished.")
